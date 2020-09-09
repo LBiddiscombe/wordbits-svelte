fa3e7d90c3daa4f1d76bf0a9330e1873bd853fca
Lee Biddiscombe (Co-op IT - Retail) <lee.biddiscombe@coop.co.uk>
2020-09-09 20:12:30 +0100

    use common words for wordsearch

--- functions/createWordsearch/createWordsearch.js
+++ functions/createWordsearch/createWordsearch.js
@@ -1,10 +1,17 @@
 const generator = require('./generator')
-const words = require('./words')
+const wordsGSL = require('./wordsGSL')
+const wordsCommon = require('./wordsCommon')
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
     //const { diagonals = true, effort = 10000, filler = null } = event.queryStringParameters
+    const wordSet = new Set([...wordsCommon, ...wordsGSL])
+    const words = [...wordSet]
+      .sort()
+      .filter((word) => word.length >= 3)
+      .map((word) => word.toUpperCase())
+      .filter((word) => /^[A-Z]*$/.test(word))
 
     return {
       statusCode: 200,
