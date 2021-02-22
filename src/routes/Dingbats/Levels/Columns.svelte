4f4d2a72289436d04de66d6a01fb97589331269e
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2021-02-22 16:32:19 +0000

    remove background colour

--- src/routes/Dingbats/Levels/Columns.svelte
+++ src/routes/Dingbats/Levels/Columns.svelte
@@ -12,7 +12,6 @@
     font-size: 2rem;
     padding: 1rem;
     justify-content: space-around;
-    background-color: rgba(255, 128, 128, 0.25);
     border: 1px solid darkgray;
     min-width: 300px;
   }
